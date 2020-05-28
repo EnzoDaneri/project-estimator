80e973b8259820896e5dd46f39141b2d455bd2e3
Enzo Daneri <enzodaneri@hotmail.com>
2020-05-28 10:56:19 -0300

    components  ready

---
+++ src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+import Table from './Table.svelte';
+import Form from './Form.svelte';
+
+
+</script>
+
+
+<main>
+<h1>Project Estimator</h1>
+<Form/>
+<Table/>
+
+</main>
+
+
+
+<style>
+main {
+	flex: 1;
+	align-content: center;
+	justify-content: center;
+	width: 80%;
+	margin: 0 auto;
+}
+
+
+</style>
